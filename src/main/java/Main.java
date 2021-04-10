290d5add0b8aa62bb7a7a09ca531f76b9276054d
Cleber Miranda Barbosa <cleber@gmail.com>
2021-04-10 15:36:15 -0400

    Fixed Employee issue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Employee employee1 = new Employee("1", "Cleber");
+        Employee employee2 = new Employee("2", "Camila");
+
+
+        System.out.printf("User1: %s", employee1);
+        System.out.printf("User2: %s", employee2);
+
 
     }
 }
